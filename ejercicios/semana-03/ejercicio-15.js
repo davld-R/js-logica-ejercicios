d8118f97005471586b3f0df7d784b31f77550964
Juan David Rojas <juanrojas043@gmail.com>
2025-09-19 17:04:57 -0500

    Semana 3 - Ejercicio-15

---
+++ ejercicios/semana-03/ejercicio-15.js
@@ -0,0 +1,8 @@
+//  ¡Programa lo más rápido posible! Necesitas duplicar el entero y devolverlo.
+
+function doubleInteger(i) {
+  // i will be an integer. Double it and return it.
+  return i * 2;
+}
+
+console.log(doubleInteger(2))
