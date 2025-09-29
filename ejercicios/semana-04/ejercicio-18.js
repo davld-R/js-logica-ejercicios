dd7143d1a98951c4b8071ccc65f8ed59b07656be
Juan David Rojas <juanrojas043@gmail.com>
2025-09-28 19:05:21 -0500

    Semana 4 - Ejercicio-18

---
+++ ejercicios/semana-04/ejercicio-18.js
@@ -0,0 +1,16 @@
+// Necesitamos una función que pueda transformar un número (entero) en una cadena.
+
+// ¿Qué formas de lograrlo conoces?
+
+// Ejemplos (entrada --> salida):
+
+// 123  --> "123"
+// 999  --> "999"
+// -100 --> "-100"
+
+
+function numberToString(num) {
+    return num.toString();
+}
+
+console.log(numberToString(67));
